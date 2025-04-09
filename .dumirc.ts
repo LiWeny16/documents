dc060ba464ba65a4da375425a7effd3f749f5241
LiWeny16 <a454888395@gmail.com>
2025-04-09 23:04:23 +0800

    chore: 初始化项目

---
+++ .dumirc.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  outputPath: 'docs-dist',
+  logo: '/icons/192x192.png', // 左上角 Logo（放在 public 目录）
+  themeConfig: {
+    name: 'LetShare!',
+  },
+});
---
+++ docs/Letshare/index.md
@@ -0,0 +1,9 @@
+# 安装指南
+
+欢迎使用 LetShare 组件库！
+
+## 安装
+
+```bash
+npm install letshare-ui
+```
